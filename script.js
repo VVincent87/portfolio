870fb8f23043588c958fd8b6699a83f0302acf2a
Victorien Vincent <vvincent@it-students.fr>
2021-07-08 17:02:10 +0200

    js comments

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// Set a variable for our button element.
+// Variable du bouton.
 const scrollToTopButton = document.getElementById('js-top');
 
-// Let's set up a function that shows our scroll-to-top button if we scroll beyond the height of the initial window.
+// Fonction pour afficher le bouton de retour en haut de page lorsque la fenêtre descend.
 const scrollFunc = () => {
-  // Get the current scroll value
+  // Récupération de la valeur de scroll actuelle
   let y = window.scrollY;
   
-  // If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
+  // Définition d'une classe pour afficher le bouton si la valeur de scroll est supérieure à celle définie
   if (y > 300) {
     scrollToTopButton.className = "top-link show";
   } else {
@@ -17,21 +17,20 @@ const scrollFunc = () => {
 window.addEventListener("scroll", scrollFunc);
 
 const scrollToTop = () => {
-  // Let's set a variable for the number of pixels we are from the top of the document.
+  // Création d'une variable contenant le nombre de pixels etre la location actuelle et le haut de page
   const c = document.documentElement.scrollTop || document.body.scrollTop;
   
-  // If that number is greater than 0, we'll scroll back to 0, or the top of the document.
-  // We'll also animate that scroll with requestAnimationFrame:
-  // https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+  // Si ce nombre est supérieur à 0, on retourne en haut de la page
+  // Animation ralentie du scroll avec RequestAnimationFrame
   if (c > 0) {
     window.requestAnimationFrame(scrollToTop);
-    // ScrollTo takes an x and a y coordinate.
-    // Increase the '10' value to get a smoother/slower scroll!
+    // Le scroll a beosin de coordonnées X et Y
+    // Changer le diviseur ralentit ou accélère la transition
     window.scrollTo(0, c - c / 10);
   }
 };
 
-// When the button is clicked, run our ScrolltoTop function above!
+// Appel de la fonction de retour en haut quand le bouton est cliqué
 scrollToTopButton.onclick = function(e) {
   e.preventDefault();
   scrollToTop();
